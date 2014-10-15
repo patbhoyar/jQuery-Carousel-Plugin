d01ac59f8779f65ff1e53224f6be66f542a9883f
patb <patbhoyar@gmail.com>
2014-10-16 01:28:17 +0530

    V 1.1 - Image Selection Done
    Code/Logic complete for user selected image. i.e. after implementing
    the remaining code, in V 2.0, user can click on one of the dots to go
    to that image with the selected transition and the carousel continues
    to run from that image.

--- js/carouselPlugin.js
+++ js/carouselPlugin.js
@@ -6,9 +6,9 @@
         var zindex = 999;
         var whosvisible = 1;
         var displayTitleNumber = 0;
-        var timeout;
+        var timeout, carousel;
         var settings = $.extend({
-            timeBetweenImages: 5,
+            timeBetweenImages: 3,
             transition: 'fade', //fade, slide, flash
             imageList: ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG'],
             imageTitle: ["Mee", "LA Valley during Day", "During Night", "HorshoeBend", "Some Blvd"]
@@ -18,9 +18,67 @@
             imageNumber = 0;
         }
 
-        var changeImage = function(){
+        var changeImage = function(thisImage, stopCarousel){
 
-            setInterval(function(){
+            /*
+            If thisImage is set, means, the user requested to load a particular image. So, stop the current carousel and start a new one with the requested image as the first image
+             */
+            if(thisImage != -1){
+                if((stopCarousel == 1) && (thisImage < settings.imageList.length)){
+                    clearInterval(carousel);
+                    console.log("On Top"+ getSlideonTop());
+                    var onTop = getSlideonTop();
+                    var zi = getZIndex(onTop);
+                    var nextImage = (thisImage + 1) > settings.imageList.length-1?0:thisImage+1;
+
+                    switch(settings.transition){
+                        case 'fade':
+                            if(onTop == 1){
+                                $("#carouselImage2").attr('src', 'images/'+settings.imageList[thisImage]).css({'z-index': --zindex});
+                                $("#carouselImage1").fadeOut('slow', function(){
+                                    $("#carouselImage1").css({display: 'block', 'z-index': --zindex}).attr('src', 'images/'+settings.imageList[nextImage]);
+                                    $("#imageTitle").text(settings.imageTitle[thisImage]);
+                                    whosvisible = 2;
+                                });
+                            }else{
+                                $("#carouselImage1").attr('src', 'images/'+settings.imageList[thisImage]).css({'z-index': --zindex});
+                                $("#carouselImage2").fadeOut('slow', function(){
+                                    $("#imageTitle").text(settings.imageTitle[thisImage]);
+                                    $("#carouselImage2").css({display: 'block', 'z-index': --zindex}).attr('src', 'images/'+settings.imageList[nextImage]);
+                                    whosvisible = 1;
+                                });
+                            }
+                            imageNumber = nextImage;
+                            break;
+                        case 'flash':
+                            $("#carouselImage1").attr('src', 'images/'+settings.imageList[thisImage]);
+                            clearTimeout(timeout);
+                            timeout = setTimeout(function() {
+                                $("#imageTitle").text(settings.imageTitle[thisImage]);
+                            }, 50);
+                            imageNumber = thisImage;
+                            break;
+                        case 'slide':
+                            $("#carouselImage1, #carouselImage2").css('display', 'none');
+                            $('#imageTitle').fadeOut('slow');
+                            $(".next").first().attr('src', 'images/'+settings.imageList[thisImage]);
+                            $("#slider").animate({'margin-left' : '-=1000'}, 2000, function(){
+                                $(this).find('img').removeClass('next');
+                                $(this).find('img').first().appendTo("#slider").addClass('next');
+                                $('.next').attr('src', 'images/'+settings.imageList[nextImage]);
+                                $("#imageTitle").css('display', 'block').text(settings.imageTitle[thisImage]);
+                                $(this).removeAttr('style');
+                            });
+                            imageNumber = nextImage;
+                            break;
+                    }
+                }
+            }
+
+            /*
+            * This is the regular Carousel
+            */
+            carousel = setInterval(function(){
                 if(++imageNumber > settings.imageList.length-1){
                     imageNumber = 0;
                 }
@@ -31,10 +89,10 @@
                     displayTitleNumber = imageNumber-1;
                 }
 
+                if(thisImage != -1){console.log(imageNumber+", title: "+displayTitleNumber);}
+
                 switch(settings.transition){
                     case 'fade':
-                        console.log(imageNumber);
-                        console.log(displayTitleNumber);
                         $("#imageTitle").fadeOut('slow');
                         if(whosvisible === 1){
                             $("#carouselImage1").fadeOut('slow', function(){
@@ -70,12 +128,25 @@
                         break;
                 }
             }, settings.timeBetweenImages*1000);
+
         };
+
+        function getSlideonTop(){
+            if($("#carouselImage1").css('z-index') > $("#carouselImage2").css('z-index'))
+                return 1;
+            else
+                return 2;
+        }
+
+        function getZIndex(ofSlide){
+            return $("#carouselImage"+ofSlide).css('z-index');
+        }
 
         $("#carouselImage1").attr('src', 'images/'+settings.imageList[0]);
         $("#carouselImage2").attr('src', 'images/'+settings.imageList[1]);
         $("#imageTitle").text(settings.imageTitle[0]);
 
-        changeImage();
+        changeImage(-1);
+        return changeImage;
     };
 })(jQuery);
